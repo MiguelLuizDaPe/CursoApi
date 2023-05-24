849b0976cb49f96a0ad3094d68b90205001ace79
Miguel Luiz <111905998+MiguelLuizDaPe@users.noreply.github.com>
2023-05-24 18:09:23 -0300

    Add files via upload

---
+++ src/Curso.Api/Data.cs
@@ -0,0 +1,31 @@
+using Curso.Api.Entities;
+
+namespace Curso.Api{
+    public class Data{
+        public List<Customer> Customers{get; set;}
+        static private Data _data;
+
+        private Data(){
+            this.Customers = new List<Customer>{
+                new Customer{
+                    Id = 1,
+                    Name = "Pedro",
+                    Cpf = "123456789123"
+                },
+                new Customer{
+                    Id = 2,
+                    Name = "Pepino",
+                    Cpf = "123999789993"
+                }
+            };
+        }
+
+        public static Data getData(){
+            if (_data == null){
+                _data = new Data();
+            }
+            return _data;
+        }
+
+    }
+}
---
+++ src/Curso.Api/Entities/Customer.cs
@@ -0,0 +1,7 @@
+namespace Curso.Api.Entities;
+
+public class Customer{
+    public int Id {get; set;}
+    public string Name {get; set;} = "";
+    public string Cpf {get; set;} = string.Empty;
+}
