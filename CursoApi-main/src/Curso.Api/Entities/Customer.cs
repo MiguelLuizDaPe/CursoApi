7f1ed924a59828761611f34f1ff13ef7d1e3651f
Miguel Luiz <111905998+MiguelLuizDaPe@users.noreply.github.com>
2023-05-28 17:28:38 -0300

    Delete CursoApi-main directory

--- CursoApi-main/src/Curso.Api/Entities/Customer.cs
+++
@@ -1,7 +0,0 @@
-namespace Curso.Api.Entities;
-
-public class Customer{
-    public int Id {get; set;}
-    public string Name {get; set;} = "";
-    public string Cpf {get; set;} = string.Empty;
-}
